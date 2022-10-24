ad4660a2ed5a3a02d6ae195ded7bc586ad318ac2
amranhakim10 <amranhakim9@gmail.com>
2022-10-24 21:44:41 +0700

    Initial commit

--- include/creator.php
+++ include/creator.php
@@ -1,17 +1,17 @@
 <div class="line-page-bottom"></div>
 <div class="footer-wrap pd-20 mb-20 card-box" style="text-align: center;">
     <?php if ($resultSetting['company'] == NULL) { ?>
-    Copyright © 2022 - Company All Rights Reserved.
+        Copyright © 2022 - Company All Rights Reserved.
     <?php } else { ?>
-    <?= $resultSetting['company'] ?>
+        <?= $resultSetting['company'] ?>
     <?php } ?>
     <a class="font-w600" href="mailto:info@hellos-id.com" target="_blank">
 
     </a>
     <br>
     <?php if ($resultSetting['version'] == NULL) { ?>
-    <b>Version -</b>
+        <b>Version -</b>
     <?php } else { ?>
-    <b><?= $resultSetting['version'] ?> - <?= $resultSetting['type'] ?></b>
+        <b><?= $resultSetting['version'] ?> - <?= $resultSetting['type'] ?></b>
     <?php } ?>
 </div>
