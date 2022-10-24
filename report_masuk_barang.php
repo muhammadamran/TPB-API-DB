5e173cab53672da5dd0abef819496f99dc68bfa2
amranhakim10 <amranhakim9@gmail.com>
2022-10-24 22:39:42 +0700

    Initial commit

--- report_masuk_barang.php
+++ report_masuk_barang.php
@@ -191,11 +191,7 @@ $data = json_decode($content, true);
                                     <th rowspan="2" style="text-align: center;">Jumlah</th>
                                     <th rowspan="2" style="text-align: center;">Nilai Barang</th>
                                     <th rowspan="2" style="text-align: center;">Tanggal & Waktu Masuk</th>
-                                    <th colsapn="2" style="text-align: center;">
-                                        <center>
-                                            Petugas Penerima
-                                        </center>
-                                    </th>
+                                    <th colsapn="2" style="text-align: center;">Petugas Penerima</th>
                                 </tr>
                                 <tr>
                                     <th style="text-align: center;">Jenis Dok. Pabean PLB</th>
