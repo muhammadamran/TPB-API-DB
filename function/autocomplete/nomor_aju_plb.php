1f9ff496546007b6089bf209505d3df350d28827
amranhakim10 <amranhakim9@gmail.com>
2022-10-25 11:49:00 +0700

    Initial commit

--- function/autocomplete/nomor_aju_plb.php
+++ function/autocomplete/nomor_aju_plb.php
@@ -1,1 +1,9 @@
-test
+<?php
+include "include/connection.php";
+include "include/api.php";
+
+$content = get_content($resultAPI['url_api'] . 'gmBarangMasuk.php?function=get_auto_noAJU');
+$data = json_decode($content, true);
+
+var_dump($data);
+exit;
