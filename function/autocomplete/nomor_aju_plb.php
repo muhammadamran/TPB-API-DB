5f35d11847e02a366c6fb750043fc509e3713e6e
amranhakim10 <amranhakim9@gmail.com>
2022-10-25 12:03:51 +0700

    Initial commit

--- function/autocomplete/nomor_aju_plb.php
+++ function/autocomplete/nomor_aju_plb.php
@@ -8,26 +8,26 @@ $dbcon = new mysqli($dbhost, $dbusername, $dbpassword, $dbname) or die(mysqli_co
 
 $searchTerm = $_GET['term']; // Menerima kiriman data dari inputan pengguna
 
-// $dataAPI = $dbcon->query("SELECT * FROM api ORDER BY id ASC LIMIT 1");
-// $resultAPI = mysqli_fetch_array($dataAPI);
+$dataAPI = $dbcon->query("SELECT * FROM api ORDER BY id ASC LIMIT 1");
+$resultAPI = mysqli_fetch_array($dataAPI);
 
-// function get_content($URL)
-// {
-//     $ch = curl_init();
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//     curl_setopt($ch, CURLOPT_URL, $URL);
-//     $data = curl_exec($ch);
-//     curl_close($ch);
-//     return $data;
-// }
+function get_content($URL)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $URL);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
 
-// $content = get_content($resultAPI['url_api'] . 'gmBarangMasuk.php?function=get_auto_noAJU');
-// $data = json_decode($content, true);
+$content = get_content($resultAPI['url_api'] . 'gmBarangMasuk.php?function=get_auto_noAJU');
+$data = json_decode($content, true);
 
-// foreach ($data['result'] as $row) {
-//     $row['NOMOR_AJU'];
-// }
-// echo json_encode($data['result']);
+foreach ($data['result'] as $row) {
+    $list[] = $row['NOMOR_AJU'];
+}
+echo json_encode($list);
 
 $sql = $dbcon->query("SELECT * FROM referensi_asal_barang WHERE URAIAN_ASAL_BARANG LIKE '%" . $searchTerm . "%' ORDER BY ID ASC");
 while ($row = mysqli_fetch_array($sql)) {
