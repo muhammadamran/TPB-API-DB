9caee5f997691694622f94abdb8f49a22b0024ad
amranhakim10 <amranhakim9@gmail.com>
2022-10-25 11:48:25 +0700

    Initial commit

--- function/autocomplete/nomor_aju_plb.php
+++ function/autocomplete/nomor_aju_plb.php
@@ -1,13 +1,1 @@
-<?php
-include "include/connection.php";
-include "include/api.php";
-
-$dataSetRealTime = $dbcon->query("SELECT * FROM tbl_realtime ORDER BY id DESC LIMIT 1");
-$resultSetRealTime = mysqli_fetch_array($dataSetRealTime);
-echo $SetTime = $resultSetRealTime['reload'];
-
-$content = get_content($resultAPI['url_api'] . 'gmBarangMasuk.php?function=get_auto_noAJU');
-$data = json_decode($content, true);
-
-var_dump($data);
-exit;
+test
