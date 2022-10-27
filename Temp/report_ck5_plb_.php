40b19e315317e414f390308c2731789d5f777efe
amranhakim10 <amranhakim9@gmail.com>
2022-10-27 09:22:35 +0700

    Initial commit

--- Temp/report_ck5_plb_.php
+++ Temp/report_ck5_plb_.php
@@ -1,16 +1,14 @@
 <?php
 include "include/connection.php";
-include "include/restrict.php";
 include "include/head.php";
 include "include/alert.php";
-include "include/top-header.php";
-include "include/top-sidebar.php";
-// include "include/sidebar.php";
+// include "include/top-header.php";
+// include "include/top-sidebar.php";
 include "include/cssDatatables.php";
 include "include/cssForm.php";
 ?>
 <!-- begin #content -->
-<div id="content" class="nav-top-content">
+<div id="content" class="nav-top-content" style="padding: 15px;margin-top: -55px;">
     <div class="page-title-css">
         <div>
             <h1 class="page-header-css">
@@ -44,9 +42,8 @@ include "include/cssForm.php";
             align-content: center;
             color: #1d2226;
         }
-
         #id-fl {
-            background: #fff;
+            background: linear-gradient(45deg, #00acac, #8753de);
             border-radius: 5px;
             /* margin-left: -10px; */
             margin-bottom: 10px;
@@ -55,27 +52,59 @@ include "include/cssForm.php";
             display: grid;
             justify-content: center;
             align-content: center;
-            color: #1d2226;
+            color: #fff;
         }
     </style>
-    <div class="row">
+    <div class="row" style="display: flex;justify-content: space-between;">
         <div class="col-xl-6">
             <div class="panel panel-inverse" data-sortable-id="ui-icons-1">
                 <a href="https://itinventory-sarinah.com/report_ck5_plb_data.php" id="id-fr">
-                    <i class="fa-solid fa-table"></i>
-                    <font style="font-size: 9px; margin-top: 9px">Data CK5 PLB</font>
+                    <i class="fa-solid fa-table"></i> <font style="font-size: 9px; margin-top: 9px">Data CK5 PLB</font>
                 </a>
             </div>
         </div>
         <div class="col-xl-6">
             <div class="panel panel-inverse" data-sortable-id="ui-icons-1">
-                <a href="http://plb.itinventory-sarinah.com:8091/report_ck5_plb.php?username=<?= $_SESSION['username']; ?>" target="_blank" id="id-fl">
-                    <i class="fa-solid fa-upload"></i>
-                    <font style="font-size: 8px; margin-top: 10px">Upload CK5 PLB</font>
+                <a href="http://plb.itinventory-sarinah.com:8091/report_ck5_plb.php" id="id-fl">
+                    <i class="fa-solid fa-upload"></i> <font style="font-size: 8px; margin-top: 10px">Upload CK5 PLB</font>
                 </a>
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="panel panel-inverse" data-sortable-id="ui-icons-1">
+                <div class="panel-heading">
+                    <h4 class="panel-title"><i class="fas fa-upload"></i> Upload Data CK5 PLB</h4>
+                    <?php include "include/panel-row.php"; ?>
+                </div>
+                <div class="panel-body text-inverse">
+                    <form action="report_ck5_plb_upload.php" method="post" enctype="multipart/form-data">
+                        <div class="row" style="display: flex;align-items: center;">
+                            <div class="col-xl-6">
+                                <div class="form-group">
+                                    <div style="margin-bottom: 10px;justify-content: center;align-items: center;display: flex;">
+                                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Microsoft_Office_Excel_%282019%E2%80%93present%29.svg/826px-Microsoft_Office_Excel_%282019%E2%80%93present%29.svg.png" style="width: 15%;">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="form-group">
+                                    <label>Upload Excel File CK5 PLB:</label>
+                                    <input type="hidden" class="form-control" name="username" value="<?= $_GET['username'] ?>" required>
+                                    <input type="file" class="form-control" name="file_upload" required>
+                                </div>
+                            </div>
+                            <div class="col-xl-12">
+                                <button type="submit" class="btn btn-block btn-primary" value="Upload"><i class="fas fa-upload"></i> Upload</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- end row -->
     <?php include "include/creator.php"; ?>
 </div>
