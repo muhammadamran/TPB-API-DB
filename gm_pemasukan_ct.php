09d90042c3fd84aa4db041aafac29e476b3926e3
amranhakim10 <amranhakim9@gmail.com>
2022-10-27 17:25:52 +0700

    Initial commit

--- gm_pemasukan_ct.php
+++ gm_pemasukan_ct.php
@@ -117,7 +117,7 @@ if (isset($_POST['show_all'])) {
                                 <thead>
                                     <tr>
                                         <th width="1%">No.</th>
-                                        <th width="1%" class="no-sort" style="text-align: center;">
+                                        <th class="no-sort" style="text-align: center;">
                                             <input type="checkbox">
                                         </th>
                                         <th width="1%">#</th>
@@ -130,7 +130,8 @@ if (isset($_POST['show_all'])) {
                                     <?php
                                     $data = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='" . $_GET['AJU'] . "' AND ID='" . $_GET['ID'] . "' ORDER BY ID ASC LIMIT 1");
                                     $result = mysqli_fetch_array($data);
-                                    $t_exp = explode('X', $result['UKURAN'])
+                                    $t_botol = explode('X', $result['UKURAN']);
+                                    $t_liter = str_replace('Ltr', '', $t_botol[1]);
                                     ?>
                                     <?php for ($i = 1; $i <= $_GET['LOOP']; $i++) { ?>
                                         <tr>
@@ -145,10 +146,10 @@ if (isset($_POST['show_all'])) {
                                                 <font><?= $result['UKURAN']; ?></font>
                                             </td>
                                             <td style="text-align: center;">
-                                                <?= $t_exp[0]; ?> Botol
+                                                <?= $t_botol[0]; ?> Botol
                                             </td>
                                             <td style="text-align: center;">
-                                                <?= $t_exp[1]; ?> Liter
+                                                <?= $t_liter; ?> Liter
                                             </td>
                                         </tr>
                                     <?php } ?>
