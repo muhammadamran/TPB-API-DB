f67715f68b038e528109c0dd9f135ec186921fcf
amranhakim10 <amranhakim9@gmail.com>
2022-10-27 22:00:48 +0700

    Initial commit

--- gm_pemasukan_ct.php
+++ gm_pemasukan_ct.php
@@ -215,7 +215,7 @@ if (isset($_POST['show_all'])) {
                                             </div>
                                         </div>
                                     </th>
-                                    <th class="no-sort" style="text-align: center;">#</th>
+                                    <th width="1%" class="no-sort" style="text-align: center;">#</th>
                                     <th style="text-align: center;">Total Botol</th>
                                     <th style="text-align: center;">Total Liter</th>
                                 </tr>
