b05a32066514067a597f8135ea84ab1a941dd1d5
amranhakim10 <amranhakim9@gmail.com>
2022-10-27 12:01:20 +0700

    Initial commit

--- function/autocomplete/nomor_aju_plb.php
+++ function/autocomplete/nomor_aju_plb.php
@@ -7,7 +7,7 @@ $dbname = 'inxmiles_tpb';
 $dbcon = new mysqli($dbhost, $dbusername, $dbpassword, $dbname) or die(mysqli_connect_errno());
 
 $searchTerm = $_GET['term'];
-$sql = $dbcon->query("SELECT * FROM pl_header WHERE NOMOR_AJU LIKE '%" . $searchTerm . "%' ORDER BY ID ASC");
+$sql = $dbcon->query("SELECT NOMOR_AJU FROM pl_header WHERE NOMOR_AJU LIKE '%" . $searchTerm . "%' ORDER BY ID ASC");
 while ($row = mysqli_fetch_array($sql)) {
     $data[] = $row['NOMOR_AJU'];
 }
