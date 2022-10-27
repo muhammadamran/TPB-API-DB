34203e1c9d2e818e60d100ef0ca37503af3f2db0
amranhakim10 <amranhakim9@gmail.com>
2022-10-27 21:19:06 +0700

    Initial commit

--- gm_pemasukan_detail.php
+++ gm_pemasukan_detail.php
@@ -377,7 +377,7 @@ $dataBarangCek      = mysqli_fetch_array($contentBarangCek);
                                                         <td><?= $noBarang ?>. </td>
                                                         <td style="text-align: center;">
                                                             <div style="margin-left: 25px;margin-bottom: 15px;">
-                                                                <input type="checkbox" class="form-check-input" id="chk" name="CekBarang[<?= $noBarang - 1; ?>][ID]" value="<?= $row['ID'] ?>">
+                                                                <input type="checkbox" class="form-check-input" id="chk" name="CekBarang[<?= $noBarang - 1; ?>][ID]" value="<?= $row['ID'] ?>" required>
                                                             </div>
                                                         </td>
                                                         <td style="text-align: center;">
