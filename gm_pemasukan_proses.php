cf525f3040ec7252d05494fccaf7ef9c58506617
amranhakim10 <amranhakim9@gmail.com>
2022-10-28 13:47:56 +0700

    Initial commit

--- gm_pemasukan_proses.php
+++ gm_pemasukan_proses.php
@@ -14,17 +14,16 @@ foreach ($key as $row) {
         $TGL_CEK = $row['TGL_CEK'];
         $CHECKING = $row['CHECKING'];
 
-        $dataUpdate = array(
-            'STATUS' => $row['STATUS'],
-            'OPERATOR_ONE' => $row['OPERATOR_ONE'],
-            'TGL_CEK' => $row['TGL_CEK'],
-            'CHECKING' => $row['CHECKING']
-        );
-
         $CHE = $dbcon->query("UPDATE plb_barang SET STATUS='$STATUS',
                                                     OPERATOR_ONE='$OPERATOR_ONE',
                                                     TGL_CEK='$TGL_CEK',
                                                     CHECKING='$CHECKING'
                               WHERE ID='$ID'");
     }
+}
+
+if ($CHE) {
+    echo "<script>window.location.href='gm_pemasukan_detail.php?ID=$NOMOR_AJU';'_blank'</script>";
+} else {
+    echo "<script>window.location.href='gm_pemasukan_detail.php?ID=$NOMOR_AJU';'_blank'</script>";
 }
