56977323bbda0764e0c1ab6e46b693c6c4bd495d
amranhakim10 <amranhakim9@gmail.com>
2022-10-28 14:25:27 +0700

    Initial commit

--- gm_pemasukan_detail.php
+++ gm_pemasukan_detail.php
@@ -264,12 +264,12 @@ $dataBarangAll    = mysqli_fetch_array($contentBarangAll);
                                                             <input type="hidden" name="CekBarang[<?= $noBarang - 1; ?>][TGL_CEK]" value="<?= date('Y-m-d H:m:i') ?>">
                                                             <?php if ($rowBarang['KODE_BARANG'] != NULL) { ?>
                                                                 <?php if ($rowBarang['CHECKING'] == 'DONE') { ?>
-                                                                <?php } else { ?>
                                                                     <a href="#" data-toggle="modal" class="btn btn-sm btn-custom btn-sucess">
                                                                         <i class="fas fa-check-circle" style="font-size: 22px;"></i>
                                                                         <br>
                                                                         Cek <?= $pcs ?> CT
                                                                     </a>
+                                                                <?php } else { ?>
                                                                     <?php if ($pcs == 0) { ?>
                                                                         <!-- No QTY -->
                                                                         <a href="#" data-toggle="modal" class="btn btn-sm btn-custom btn-danger">
