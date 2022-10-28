fae51a333d38399d6235d2e5c3a9630170724aa6
amranhakim10 <amranhakim9@gmail.com>
2022-10-28 13:53:03 +0700

    Initial commit

--- gm_pemasukan_proses.php
+++ gm_pemasukan_proses.php
@@ -1,29 +1,9 @@
 <?php
 include "include/connection.php";
 
-$key = $_POST['CekBarang'];
-// var_dump($key);
-// exit;
-foreach ($key as $row) {
-    if (@$row['ID']) {
-        $ID = $row['ID'];
-        $NOMOR_AJU = $row['NOMOR_AJU'];
-        $KODE_BARANG = $row['KODE_BARANG'];
-        $TOTAL_BOTOL = $row['TOTAL_BOTOL'];
-        $TOTAL_LITER = $row['TOTAL_LITER'];
-        $STATUS = $row['STATUS'];
-        $OPERATOR_ONE = $row['OPERATOR_ONE'];
-        $TGL_CEK = $row['TGL_CEK'];
-        $CHECKING = $row['CHECKING'];
-
-        $CHE = $dbcon->query("UPDATE plb_barang SET STATUS='$STATUS',
-                                                    OPERATOR_ONE='$OPERATOR_ONE',
-                                                    TGL_CEK='$TGL_CEK',
-                                                    CHECKING='$CHECKING'
-                              WHERE ID='$ID'");
-    }
-}
-$NOMOR_AJU = $row['NOMOR_AJU'];
+$NOMOR_AJU = $key['NOMOR_AJU'];
+var_dump($NOMOR_AJU);
+exit;
 if ($CHE) {
     echo "<script>window.location.href='gm_pemasukan_detail.php?ID=$NOMOR_AJU';'_blank'</script>";
 } else {
