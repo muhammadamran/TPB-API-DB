38ae509bc5ea0e48f6a1f4ba5c106bab10e00386
amranhakim10 <amranhakim9@gmail.com>
2022-10-28 12:33:26 +0700

    Initial commit

--- gm_pemasukan_ct.php
+++ gm_pemasukan_ct.php
@@ -10,7 +10,9 @@ include "include/cssForm.php";
 
 // Submit CT
 if (isset($_POST["ct_submit"])) {
-    $keyy = @$_POST['ID_BARANG'];
+    $keyy = $_POST['CekBarangBotol'];
+    var_dump($keyy);
+    exit;
     // CEK CT
     $cekCT = $dbcon->query("SELECT * FROM plb_barang_ct WHERE ID_BARANG='$keyy'");
     $dataCT    = mysqli_fetch_array($cekCT);
