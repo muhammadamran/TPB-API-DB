4e1cf805cd313872f3f7a5aa8df08415cbc1ce86
amranhakim10 <amranhakim9@gmail.com>
2022-10-28 17:53:28 +0700

    Initial commit

--- gm_pemasukan_detail.php
+++ gm_pemasukan_detail.php
@@ -279,7 +279,7 @@ $dataBarangAll    = mysqli_fetch_array($contentBarangAll);
                                                                         </a>
                                                                     <?php } else { ?>
                                                                         <!-- Check -->
-                                                                        <a href="gm_pemasukan_ct.php?ID_BARANG=<?= $rowBarang['ID'] ?>&aksi=SubmitCT" target="_blank" onClick="return openWindowReload(this)" class="btn btn-sm btn-custom btn-warning">
+                                                                        <a href="gm_pemasukan_ct.php?ID_BARANG=<?= $rowBarang['ID'] ?>&aksi=SubmitCT" target="_blank" onClick="openWindowReload(this)"> class="btn btn-sm btn-custom btn-warning">
                                                                             <i class="fas fa-boxes" style="font-size: 22px;"></i>
                                                                             <br>
                                                                             Cek <?= $pcs ?> CT
@@ -449,11 +449,8 @@ $dataBarangAll    = mysqli_fetch_array($contentBarangAll);
 </script>
 <script>
     function openWindowReload(link) {
-        setTimeout(function() {
-            var href = link.href;
-            window.open(href, '_blank');
-            document.location.reload(true)
-        }, 5000);
-        return false;
+        var href = link.href;
+        window.open(href, '_blank');
+        document.location.reload(true)
     }
 </script>
