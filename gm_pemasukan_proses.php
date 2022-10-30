d46ca398f85aaa75fc4cdab74717a7c050f42ef9
amranhakim10 <amranhakim9@gmail.com>
2022-10-30 23:04:57 +0700

    Initial commit

--- gm_pemasukan_proses.php
+++ gm_pemasukan_proses.php
@@ -8,43 +8,36 @@ include "include/sidebar.php";
 include "include/cssDatatables.php";
 
 if (isset($_GET["aksi"]) == 'SubmitCT') {
-    $meOK           = $_SESSION['username'];
+    $AJU            = $_GET['AJU'];
     $InputDate      = date('Y-m-d h:m:i');
 
-    $key = ['CekBarang'];
-    foreach ($key as $row) {
-        if (@$row['ID']) {
-            // ID_BARANG
-            $ID                 = $row['ID'];
-            // <!-- PLB_BARANG_CT -->
-            $NOMOR_AJU          = $_POST['NOMOR_AJU'];
-            $KODE_BARANG        = $_POST['KODE_BARANG'];
-            // <!-- PLB_BARANG -->
-            // <!-- STATUS,OPERATOR_ONE,TGL_CEK -->
-            $STATUS             = $_POST['STATUS'];
-            $OPERATOR_ONE       = $_POST['OPERATOR_ONE'];
-            $TGL_CEK            = $_POST['TGL_CEK'];
-            $CHECKING           = $_POST['CHECKING'];
-            // <!-- STATUS_CT,DATE_CT,TOTAL_BOTOL_AKHIR,TOTAL_LITER_AKHIR,TOTAL_CT_AKHIR -->
-            $STATUS_CT          = $_POST['STATUS_CT'];
-            $DATE_CT            = $_POST['DATE_CT'];
-            $TOTAL_BOTOL        = $_POST['TOTAL_BOTOL'];
-            $TOTAL_BOTOL_AKHIR  = $_POST['TOTAL_BOTOL_AKHIR'];
-            $TOTAL_LITER        = $_POST['TOTAL_LITER'];
-            $TOTAL_LITER_AKHIR  = $_POST['TOTAL_LITER_AKHIR'];
-            $TOTAL_CT           = $_POST['TOTAL_CT'];
-            $TOTAL_CT_AKHIR     = $_POST['TOTAL_CT_AKHIR'];
-            $dataTable = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='$NOMOR_AJU' AND ID='$ID' AND CHECKING IS NULL");
-            if (mysqli_num_rows($dataTable) > 0) {
-                while ($rowWhile = mysqli_fetch_array($dataTable)) {
-                    echo $rowWhile['ID'];
-                    exit;
-                }
-                // CLOSE WHILE
-            }
-            // CLOSE IF MYSQLI ROW
+    // ID_BARANG
+    $ID                 = $row['ID'];
+    // <!-- PLB_BARANG_CT -->
+    $NOMOR_AJU          = $_POST['NOMOR_AJU'];
+    $KODE_BARANG        = $_POST['KODE_BARANG'];
+    // <!-- PLB_BARANG -->
+    // <!-- STATUS,OPERATOR_ONE,TGL_CEK -->
+    $STATUS             = $_POST['STATUS'];
+    $OPERATOR_ONE       = $_POST['OPERATOR_ONE'];
+    $TGL_CEK            = $_POST['TGL_CEK'];
+    $CHECKING           = $_POST['CHECKING'];
+    // <!-- STATUS_CT,DATE_CT,TOTAL_BOTOL_AKHIR,TOTAL_LITER_AKHIR,TOTAL_CT_AKHIR -->
+    $STATUS_CT          = $_POST['STATUS_CT'];
+    $DATE_CT            = $_POST['DATE_CT'];
+    $TOTAL_BOTOL        = $_POST['TOTAL_BOTOL'];
+    $TOTAL_BOTOL_AKHIR  = $_POST['TOTAL_BOTOL_AKHIR'];
+    $TOTAL_LITER        = $_POST['TOTAL_LITER'];
+    $TOTAL_LITER_AKHIR  = $_POST['TOTAL_LITER_AKHIR'];
+    $TOTAL_CT           = $_POST['TOTAL_CT'];
+    $TOTAL_CT_AKHIR     = $_POST['TOTAL_CT_AKHIR'];
+    $dataTable = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='$NOMOR_AJU' AND CHECKING IS NULL");
+    if (mysqli_num_rows($dataTable) > 0) {
+        while ($rowWhile = mysqli_fetch_array($dataTable)) {
+            echo $rowWhile['ID'];
+            exit;
         }
-        // CLOSE IF ID CEKBARANG
+        // CLOSE WHILE
     }
-    // CLOSE FOREACH
+    // CLOSE IF MYSQLI ROW
 }
