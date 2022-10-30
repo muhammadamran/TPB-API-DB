5ad75720a882d39a32519995d53636dab7b7a58f
amranhakim10 <amranhakim9@gmail.com>
2022-10-30 16:45:16 +0700

    Initial commit

--- gm_pemasukan_ct.php
+++ gm_pemasukan_ct.php
@@ -632,7 +632,7 @@ $NA_LITER               = mysqli_fetch_array($contentNA_LITER);
                                                                 <div style="display: flex;">
                                                                     <i class="fa-solid fa-bottle-droplet" style="color: #ffd900;font-size:221px"></i>
                                                                     <div class="card-body" style="margin-left: 0px;">
-                                                                        <h4 class="card-title">1 CT Broken</h4>
+                                                                        <h4 class="card-title">ID CT <?= $row['NOMOR_AJU']; ?></h4>
                                                                         <p class="card-text">Total Botol: <?= $row['TOTAL_BOTOL']; ?><br>Total Liter: <?= $row['TOTAL_BOTOL'] * $row['TOTAL_LITER']; ?></p>
                                                                         <a href="javascript:;" class="btn btn-sm btn-default">Jumlah Botol Saat Ini: <?= $row['TOTAL_BOTOL']; ?> Botol</a>
                                                                         <div>
@@ -684,7 +684,7 @@ $NA_LITER               = mysqli_fetch_array($contentNA_LITER);
                                                                 <div style="display: flex;">
                                                                     <i class="fa-solid fa-bottle-droplet" style="color: #90ca4b;font-size:221px"></i>
                                                                     <div class="card-body" style="margin-left: 0px;">
-                                                                        <h4 class="card-title">1 CT Broken</h4>
+                                                                        <h4 class="card-title">ID CT <?= $row['NOMOR_AJU']; ?></h4>
                                                                         <p class="card-text">Total Botol: <?= $row['TOTAL_BOTOL']; ?><br>Total Liter: <?= $row['TOTAL_BOTOL'] * $row['TOTAL_LITER']; ?></p>
                                                                         <a href="javascript:;" class="btn btn-sm btn-default">Jumlah Botol Saat Ini: <?= $row['TOTAL_BOTOL']; ?> Botol</a>
                                                                         <div>
@@ -736,7 +736,7 @@ $NA_LITER               = mysqli_fetch_array($contentNA_LITER);
                                                                 <div style="display: flex;">
                                                                     <i class="fa-solid fa-bottle-droplet" style="color: #2d353c;font-size:221px"></i>
                                                                     <div class="card-body" style="margin-left: 0px;">
-                                                                        <h4 class="card-title">1 CT Broken</h4>
+                                                                        <h4 class="card-title">ID CT <?= $row['NOMOR_AJU']; ?></h4>
                                                                         <p class="card-text">Total Botol: <?= $row['TOTAL_BOTOL']; ?><br>Total Liter: <?= $row['TOTAL_BOTOL'] * $row['TOTAL_LITER']; ?></p>
                                                                         <a href="javascript:;" class="btn btn-sm btn-default">Jumlah Botol Saat Ini: <?= $row['TOTAL_BOTOL']; ?> Botol</a>
                                                                         <div>
@@ -788,7 +788,7 @@ $NA_LITER               = mysqli_fetch_array($contentNA_LITER);
                                                                 <div style="display: flex;">
                                                                     <i class="fa-solid fa-bottle-droplet" style="color: #f59c1a;font-size:221px"></i>
                                                                     <div class="card-body" style="margin-left: 0px;">
-                                                                        <h4 class="card-title">1 CT Broken</h4>
+                                                                        <h4 class="card-title">ID CT <?= $row['NOMOR_AJU']; ?></h4>
                                                                         <p class="card-text">Total Botol: <?= $row['TOTAL_BOTOL']; ?><br>Total Liter: <?= $row['TOTAL_BOTOL'] * $row['TOTAL_LITER']; ?></p>
                                                                         <a href="javascript:;" class="btn btn-sm btn-default">Jumlah Botol Saat Ini: <?= $row['TOTAL_BOTOL']; ?> Botol</a>
                                                                         <div>
