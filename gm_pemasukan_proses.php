d26a12bbf4e1983cd34f368bab2f0d48754d644f
amranhakim10 <amranhakim9@gmail.com>
2022-10-30 21:49:20 +0700

    Initial commit

--- gm_pemasukan_proses.php
+++ gm_pemasukan_proses.php
@@ -3,6 +3,7 @@ include "include/connection.php";
 
 $key = $_POST['CekBarang'];
 foreach ($key as $row) {
+    // ID_BARANG
     $ID             = $row['ID'];
     $NOMOR_AJU      = $row['NOMOR_AJU'];
     $KODE_BARANG    = $row['KODE_BARANG'];
@@ -10,8 +11,7 @@ foreach ($key as $row) {
     $OPERATOR_ONE   = $row['OPERATOR_ONE'];
     $TGL_CEK = $row['TGL_CEK'];
     $CHECKING = $row['CHECKING'];
-
-    $sql = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='$NOMOR_AJU'");
+    $meOK         = $_SESSION['username'];
 
     $STATUS_CT = $row['STATUS_CT'];
     $DATE_CT = $row['DATE_CT'];
@@ -21,46 +21,70 @@ foreach ($key as $row) {
     $TOTAL_LITER_AKHIR = $row['TOTAL_LITER_AKHIR'];
     $TOTAL_CT = $row['TOTAL_CT'];
     $TOTAL_CT_AKHIR = $row['TOTAL_CT_AKHIR'];
-}
 
-$update = $dbcon->query("UPDATE plb_barang SET STATUS='$STATUS',
-                                               OPERATOR_ONE='$OPERATOR_ONE',
-                                               TGL_CEK='$TGL_CEK',
-                                               OPERATOR_ONE='$OPERATOR_ONE',
-                                               CHECKING='$CHECKING'
-                                            WHERE NOMOR_AJU='$NOMOR_AJU'");
+    $sql = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='$NOMOR_AJU'");
+    if (mysqli_num_rows($dataTable) > 0) {
+        while ($row = mysqli_fetch_array($dataTable)) {
 
+            $sql = $dbcon->query("SELECT * FROM plb_barang WHERE ID='$row[ID]'");
+            $row_ct = mysqli_fetch_array($sql);
+            $jml_pcs         = $row_ct['JUMLAH_SATUAN'];
+            $pcs             = str_replace(".0000", "", "$jml_pcs");
+            // TOTAL BOTOL
+            $botol           = explode('X', $row_ct['UKURAN']);
+            $t_botol         = $botol[0];
+            // TOTAL LITER
+            $liter           =  $botol[1];
+            $r_liter         = str_replace(['LTR', 'LTr', 'Ltr', 'ltr'], ['', '', '', ''], $liter);
+            $t_liter         = str_replace(',', '.', $r_liter);
 
-if ($update) {
+            if ($dataCT['ID_BARANG'] == NULL) {
 
-    $contentBarang = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='$NOMOR_AJU'");
-    $dataBarang    = mysqli_fetch_array($contentBarang);
-    $jml_pcs = $dataBarang['JUMLAH_SATUAN'];
-    $pcs = str_replace(".0000", "", "$jml_pcs");
-
-    // TOTAL BOTOL
-    $botol = explode('X', $dataBarang['UKURAN']);
-    $t_botol = $botol[0];
-    // TOTAL LITER
-    $liter =  $botol[1];
-    $r_liter = str_replace(['LTR', 'LTr', 'Ltr', 'ltr'], ['', '', '', ''], $liter);
-    $t_liter = str_replace(',', '.', $r_liter);
-
-    for ($i = 0; $i < $pcs; $i++) {
-        $sql = $dbcon->query("INSERT INTO plb_barang_ct 
+                // INSERT TO PLB_BARANG_CT
+                for ($i = 0; $i < $pcs; $i++) {
+                    $query = $dbcon->query("INSERT INTO plb_barang_ct 
                             (ID,NOMOR_AJU,ID_BARANG,KODE_BARANG,TOTAL_BOTOL,TOTAL_LITER)
                             VALUES
-                            ('','$dataBarang[NOMOR_AJU]','$ID','$dataBarang[KODE_BARANG]','$t_botol','$t_liter')
+                            ('','$row_ct[NOMOR_AJU]','$keyy','$row_ct[KODE_BARANG]','$t_botol','$t_liter')
                             ");
-    }
+                }
 
-    if ($sql) {
-        echo "<script>window.location.href='gm_pemasukan_ct.php?ID=$ID';'_blank'</script>";
-    } else {
-        echo "<script>window.location.href='gm_pemasukan_ct.php?InputIconFailed=true';</script>";
-    }
+                $query .= $dbcon->query("UPDATE plb_barang SET STATUS='Sesuai',
+                                                  OPERATOR_ONE='$meOK',
+                                                  TGL_CEK='$InputDate',
+                                                  CHECKING='DONE',
+                                                  STATUS_CT='Complete',
+                                                  DATE_CT='$InputDate',
+                                                  BOTOL='$A_BOTOL',
+                                                  TOTAL_BOTOL_AKHIR='$TOTAL_BOTOL',
+                                                  LITER='$A_LITER',
+                                                  TOTAL_LITER_AKHIR='$TOTAL_LITER',
+                                                  TOTAL_CT_AKHIR='$TOTAL_CT'
+                            WHERE ID='$ID'");
 
-    echo "<script>window.location.href='gm_pemasukan_detail.php?ID=$NOMOR_AJU';'_blank'</script>";
-} else {
-    echo "<script>window.location.href='gm_pemasukan_detail.php?ID=$NOMOR_AJU'?status=error;'_blank'</script>";
+                // FOR AKTIFITAS
+                $me         = $_SESSION['username'];
+                $datame     = $dbcon->query("SELECT * FROM view_privileges WHERE USER_NAME='$me'");
+                $resultme   = mysqli_fetch_array($datame);
+
+                $IDUNIQme             = $resultme['USRIDUNIQ'];
+                $InputUsername        = $me;
+                $InputModul           = 'Gate In/Detail/CT';
+                $InputDescription     = $me . " Cek Barang Masuk: ID Barang Masuk" . @$_GET['ID_BARANG'];
+                $InputAction          = 'Cek Barang Masuk';
+                $InputDate            = date('Y-m-d h:m:i');
+
+                $query .= $dbcon->query("INSERT INTO tbl_aktifitas
+                           (id,IDUNIQ,username,modul,description,action,date_created)
+                           VALUES
+                           ('','$IDUNIQme','$InputUsername','$InputModul','$InputDescription','$InputAction','$InputDate')");
+            }
+
+            if ($query) {
+                echo "<script>window.location.href='gm_pemasukan_ct.php?ID=$ID_BARANG&Alert=CekBarangMasuk&AJU=$NOMOR_AJU&AlertBroken=Success';</script>";
+            } else {
+                echo "<script>window.location.href='gm_pemasukan_ct.php?ID=$ID_BARANG&Alert=CekBarangMasuk&AJU=$NOMOR_AJU&AlertBroken=Failed';</script>";
+            }
+        }
+    }
 }
