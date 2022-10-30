7bc95a00bb83fdcebf64f1af80a5e82a90fcae84
amranhakim10 <amranhakim9@gmail.com>
2022-10-30 10:03:50 +0700

    Initial commit

--- include/modules/GateMandiri/menu.php
+++ include/modules/GateMandiri/menu.php
@@ -1,3 +1,11 @@
+<?php
+// TOTAL GATE IN
+$gate_in = $dbcon->query("SELECT COUNT(NOMOR_AJU) AS total_in FROM plb_barang WHERE STATUS_IN IS NULL");
+$result_in = mysqli_fetch_array($gate_in);
+// TOTAL GATE OUT
+$gate_out = $dbcon->query("SELECT COUNT(NOMOR_AJU) AS total_out FROM plb_barang WHERE STATUS_IN IS NOT NULL");
+$result_out = mysqli_fetch_array($gate_out);
+?>
 <li class="nav-header">GATE MANDIRI</li>
 <li class="has-sub <?= $uriSegments[1] == 'gm_pemasukan.php' ||
                         $uriSegments[1] == 'gm_pemasukan_detail.php' ||
@@ -15,13 +23,13 @@
         <li class="<?= $uriSegments[1] == 'gm_pemasukan.php' || $uriSegments[1] == 'gm_pemasukan_detail.php' ? 'active' : '' ?>">
             <a href="gm_pemasukan.php">
                 Gate In
-                <!-- <span class="label label-theme">0</span> -->
+                <span class="label label-theme"><?= $result_in['total_in'] ?></span>
             </a>
         </li>
         <li class="<?= $uriSegments[1] == 'gm_pengeluaran.php' || $uriSegments[1] == 'gm_pengeluaran_detail.php' ? 'active' : '' ?>">
             <a href="gm_pengeluaran.php">
                 Gate Out
-                <!-- <span class="label label-theme">0</span> -->
+                <span class="label label-theme"><?= $result_in['total_out'] ?></span>
             </a>
         </li>
     </ul>
