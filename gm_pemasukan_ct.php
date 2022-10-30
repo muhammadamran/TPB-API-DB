40305a10e3279d9c4132e5b804280b6fc29611da
amranhakim10 <amranhakim9@gmail.com>
2022-10-30 15:19:26 +0700

    Initial commit

--- gm_pemasukan_ct.php
+++ gm_pemasukan_ct.php
@@ -229,11 +229,11 @@ $s_CT                   = $NA_CT['p_CT'];
 // BOTOL
 $contentNA_BOTOL        = $dbcon->query("SELECT SUM(TOTAL_BOTOL) AS p_BOTOL FROM plb_barang_ct WHERE NOMOR_AJU='" . $_GET['AJU'] . "' AND ID_BARANG='" . $_GET['ID'] . "' AND STATUS_CT IS NULL ORDER BY ID", 0);
 $NA_BOTOL               = mysqli_fetch_array($contentNA_BOTOL);
-$s_BOTOL                = $NA_BOTOL['p_BOTOL'] * $s_CT;
+$s_BOTOL                = ($NA_CT['p_CT'] * $NA_BOTOL['p_BOTOL']);
 // LITER
 $contentNA_LITER        = $dbcon->query("SELECT SUM(TOTAL_LITER) AS p_LITER FROM plb_barang_ct WHERE NOMOR_AJU='" . $_GET['AJU'] . "' AND ID_BARANG='" . $_GET['ID'] . "' AND STATUS_CT IS NULL ORDER BY ID", 0);
 $NA_LITER               = mysqli_fetch_array($contentNA_LITER);
-$s_LITER                = $NA_LITER['p_LITER'] * $s_CT * $s_BOTOl;
+$s_LITER                = ($NA_CT['p_CT'] * $NA_BOTOL['p_BOTOL'] * $NA_LITER['p_LITER']);
 ?>
 <style>
     .btn-custom {
