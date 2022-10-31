e346a7aa3d5d68ee7392a22f90c4c85e5a2419a4
amranhakim10 <amranhakim9@gmail.com>
2022-10-31 08:30:12 +0700

    Initial commit

--- gm_pengeluaran_proses.php
+++ gm_pengeluaran_proses.php
@@ -13,7 +13,7 @@ if (isset($_GET["aksi"]) == 'SubmitCTT') {
     $meOK           = $_SESSION['username'];
 
     $dataTable = $dbcon->query("SELECT * FROM plb_barang WHERE NOMOR_AJU='$AJU' AND CHECKING_2 IS NULL", 0);
-    var_dump($AJU);
+    var_dump($dataTable);
     exit;
     foreach ($dataTable as $rowLine) {
         if (@$rowLine['ID']) {
