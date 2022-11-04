08ee4a36384ed11eda3850f58a1bc680316faad5
amranhakim10 <amranhakim9@gmail.com>
2022-11-04 22:17:04 +0700

    Initial commit

--- include/connection.php
+++ include/connection.php
@@ -1,9 +1,14 @@
 <?php
 // Server Prod
 $dbhost = 'localhost';
+// DOMAIN
 $dbusername = 'inxmiles_tpb';
 $dbpassword = 'Flatrone2241TPB';
 $dbname = 'inxmiles_tpb';
+// LOCAL
+// $dbusername = 'root';
+// $dbpassword = '';
+// $dbname = 'tpb';
 $dbcon = new mysqli($dbhost, $dbusername, $dbpassword, $dbname) or die(mysqli_connect_errno());
 
 // QUERY SETTING API
